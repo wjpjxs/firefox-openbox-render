3855c8d75c0e401c3fcae055ea2459d8e492dc4c
wjpjxs <167741147+wjpjxs@users.noreply.github.com>
2025-11-24 00:43:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
-export DISPLAY=:0
+[supervisord]
+nodaemon=true
+logfile=/dev/null
 
-# 启动 Xvfb 和 Openbox
-Xvfb :0 -screen 0 1280x800x24 &
-openbox &
-
-# 设置 noVNC 密码
-echo "qVXe%B;LF,m/h7j" > /app/novnc/passwd
-
-# 启动 x11vnc
-x11vnc -display :0 -forever -rfbauth /app/novnc/passwd -shared -rfbport 5900 &
-
-# 启动 noVNC
-/app/novnc/websockify/run 8080 localhost:5900 --web /app/novnc/web
+[program:startscript]
+command=/app/start.sh
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
